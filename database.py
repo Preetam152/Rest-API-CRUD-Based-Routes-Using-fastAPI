c89fbcfda2bc47e8f13a3e7b02b5e24566d028e3
Preetam152 <58979173+Preetam152@users.noreply.github.com>
2022-11-01 16:56:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 #Creates database engine
 engine = create_engine('sqlite:///todo.db')
 
+#Create declaritive base meta instance
 Base = declarative_base()
 
+#Create session local class for session maker
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 
